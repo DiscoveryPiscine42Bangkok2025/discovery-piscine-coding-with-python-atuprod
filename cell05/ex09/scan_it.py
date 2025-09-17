bc4929e5990b9fa07299021f7340d6f0ad45af2d
atuprod <67050617@kmitl.ac.th>
2025-09-17 23:45:14 +0700

    Create scan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,18 @@
+import sys
+
+def main():
+    if len(sys.argv) != 3:
+        print("none")
+        return
+
+    keyword = sys.argv[1]
+    text = sys.argv[2]
+
+    count = text.count(keyword)
+
+    if count > 0:
+        print(count)
+    else:
+        print("none")
+
+main()
