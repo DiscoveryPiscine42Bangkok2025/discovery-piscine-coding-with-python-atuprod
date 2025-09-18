e5eda583287e9d234f2b0112a1b4f68bcb7bb568
atuprod <67050617@kmitl.ac.th>
2025-09-18 22:27:45 +0700

    Create main.py

---
+++ minipro/ex01/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from checkmate_bonus import is_checkmate, is_check
+
+def main():
+    boards = [
+"""\
+R...
+.K..
+..P.
+....\
+""",
+
+"""\
+R..Q
+.K..
+..P.
+....\
+"""
+    ]
+
+    for idx, b in enumerate(boards, start=1):
+        print(f"\nBoard {idx}:\n{b}")
+        if is_checkmate(b):
+            print("Checkmate")
+        elif is_check(b):
+            print("Check")
+        else:
+            print("Safe")
+
+if __name__ == "__main__":
+    main()
